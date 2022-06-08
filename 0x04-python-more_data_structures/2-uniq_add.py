718638e17e6927ff243dec17ce4aa0e9c379c665
Olaitan Adedokun <kenney9224@gmail.com>
2022-06-08 15:03:45 +0100

    Reduce the comment character to <= 80

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# A function that adds all unique integers in a list (only once for each integer)
+# Function that adds all unique integers in a list (only once for each integer)
 def uniq_add(my_list=[]):
     return sum(list(set(my_list)))
