17c6c209cb7ce5f9cc794e402a083094d9d41650
Olaitan Adedokun <kenney9224@gmail.com>
2022-06-09 04:15:29 +0100

    Remove whitespaces

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-A function that returns a new dictionary 
+A function that returns a new dictionary
 with all values multiplied by 2
-""" 
+"""
+
+
 def multiply_by_2(a_dictionary):
-    new_dictionary = {key: value *2 for key, value in a_dictionary.items()}
+    new_dictionary = {key: value * 2 for key, value in a_dictionary.items()}
     return new_dictionary
