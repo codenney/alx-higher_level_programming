d6ce10bf101ba9e30529b3a427b3994c3a865b3b
Olaitan Adedokun <kenney9224@gmail.com>
2022-07-25 15:22:40 +0100

    Added the staticmethod keyword

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -77,6 +77,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1  # Decrease the no of instance count
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the biggest rectangle based on the area"""
         if(not isinstance(rect_1, Rectangle)):
