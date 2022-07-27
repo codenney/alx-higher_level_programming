728791e20ba9ab223fd7028d16175fe8cb161a0e
Olaitan Adedokun <kenney9224@gmail.com>
2022-07-27 18:38:52 +0100

    Got rid of pycode error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that adds 2 integers"""
 
+
 def add_integer(a, b=98):
     """Returns the addition of a and b"""
     if(not isinstance(a, int) and not isinstance(a, float)):
