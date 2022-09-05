935cd8bdb24d4853e8af8a5827e41362060f428f
Olaitan Adedokun <kenney9224@gmail.com>
2022-09-05 19:37:01 +0100

    reduced the lines

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 // A script that prints a message depending of the number of arguments passed
 
-let argvLength = process.argv.length;
-
-if (argvLength <= 2) {
-  console.log('No argument');
-} else if (argvLength === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+if (process.argv.length <= 2) console.log('No argument');
+else if (process.argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
