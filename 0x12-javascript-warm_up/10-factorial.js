76264fcf0983990a5a0502f6f309cc2c0ed6c6aa
Olaitan Adedokun <kenney9224@gmail.com>
2022-09-06 14:54:13 +0100

    Convert string to integer

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 // A script that computes and prints a factorial
 
-const num = process.argv[2];
-
-const factorial = (num) => {
-  if (!num) return 1;
-  else if (num < 0 || num === 1) return 1;
-  else return num * factorial(num - 1);
+const num = parseInt(process.argv[2]);
+const factor = (n) => {
+  if (!n) return 1;
+  else if (n < 0 || n === 1) return 1;
+  else return n * factor(n - 1);
 };
-console.log(factorial(num));
+console.log(factor(num));
