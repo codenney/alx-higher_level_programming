a2f5b17d8b1f13a6bd9910e22c9319771203ca1b
Olaitan Adedokun <kenney9224@gmail.com>
2022-09-06 14:57:44 +0100

    Remove the else if

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,8 +3,7 @@
 
 const num = parseInt(process.argv[2]);
 const factor = (n) => {
-  if (!n) return 1;
-  else if (n < 0 || n === 1) return 1;
+  if (isNaN(n) || n <= 1) return 1;
   else return n * factor(n - 1);
 };
 console.log(factor(num));
