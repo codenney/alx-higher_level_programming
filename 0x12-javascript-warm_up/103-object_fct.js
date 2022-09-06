7bfb038ffdc7cf1231428c074fb7f483fdfdbc19
Olaitan Adedokun <kenney9224@gmail.com>
2022-09-06 15:55:26 +0100

    Some clean-up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
 myObject.incr = () => {
-  myObject.value + 1;
+  myObject.value = myObject.value + 1;
 };
 myObject.incr();
 console.log(myObject);
