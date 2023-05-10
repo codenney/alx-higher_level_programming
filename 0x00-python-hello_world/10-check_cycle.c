15ab45023adbf79acd59d308c091b1fb737b4fc0
Olaitan Adedokun <kenney9224@gmail.com>
2023-05-10 15:40:30 +0100

    refactor the function

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,11 +8,14 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *one_step = list;
-	listint_t *two_step = list->next;
+	listint_t *one_step;
+	listint_t *two_step;
 
-	if (one_step == NULL || two_step == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
+
+	one_step = list;
+	two_step = list->next;
 
 	while (two_step != NULL && two_step->next != NULL)
 	{
