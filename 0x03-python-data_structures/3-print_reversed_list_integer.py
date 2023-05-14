b7f74ced6bfca88beedff0bc84466481238fc92b
Olaitan Adedokun <kenney9224@gmail.com>
2023-05-14 15:55:57 +0100

    changed to while loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,7 @@
 # A function that prints all integers of a list, in reverse order
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list)-1, -1, -1):
+    i = len(my_list) - 1
+    while i >= 0:
         print("{:d}".format(my_list[i]))
+        i -= 1
