291d8db691fd4d1a0fc95618001f638bcb1e0455
Olaitan Adedokun <kenney9224@gmail.com>
2023-05-14 23:24:25 +0100

    the max should be the first item

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,9 +5,9 @@
 
 def max_integer(my_list=[]):
     if isinstance(my_list, list):
-        max = 0
         if (len(my_list) < 1):
             return None
+        max = my_list[0]
         for num in my_list:
             if num > max:
                 max = num
