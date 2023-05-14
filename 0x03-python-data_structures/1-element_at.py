77a363b1a1b0783e8cb40dde409b8097be37ba84
Olaitan Adedokun <kenney9224@gmail.com>
2023-05-14 15:11:46 +0100

    fixed pycodestyle

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@ def element_at(my_list, idx):
     if idx > len(my_list) - 1:
         return None
     elif idx < 0:
-        return None 
+        return None
     for num in my_list:
         if my_list.index(num) == idx:
             return num
